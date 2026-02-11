991aad8dfd4657740d3d9594fd652d770d243174
s0ur4bhkumar <soul65917@gmail.com>
2026-02-11 12:45:24 +0530

    api fetch done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
 import "./styles/style.css";
-
-const city = document.getElementById('city')
-const searchBtn = document.querySelector('form > button')
-const form = document.querySelector('form')
-searchBtn.addEventListener('click', (event) => {
-  event.preventDefault()
-  if (city.validity.valid) {
-    console.log(true)
-  }
-  form.reset()
-})
+import './main.js'
---
+++ src/main.js
@@ -0,0 +1,28 @@
+const city = document.getElementById("city");
+const searchBtn = document.querySelector("form > button");
+const form = document.querySelector("form");
+
+async function getData(cityName) {
+  try {
+    const cityDetail = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?key=F2KWLQ3KTZ2TUM2EGXKH39RVJ`,
+    );
+    const data = await cityDetail.json();
+    // console.log(data);
+    return data.currentConditions.temp
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+searchBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  (async function addToPage() {
+    const temp = await getData("pune");
+    console.log(temp)
+  })()
+
+  form.reset();
+});
+
+// console.log(await temp)
