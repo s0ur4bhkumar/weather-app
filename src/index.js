2f76afe232916f5a07e534d3a728f3157896a48d
s0ur4bhkumar <soul65917@gmail.com>
2026-02-11 11:51:34 +0530

    set up the basics

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./styles/style.css";
+
+const city = document.getElementById('city')
+const searchBtn = document.querySelector('form > button')
+const form = document.querySelector('form')
+searchBtn.addEventListener('click', (event) => {
+  event.preventDefault()
+  if (city.validity.valid) {
+    console.log(true)
+  }
+  form.reset()
+})
