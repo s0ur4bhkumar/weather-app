19f1b1a185d73cfe8d91f943005f110e931cac2e
s0ur4bhkumar <soul65917@gmail.com>
2026-02-11 11:27:33 +0530

    reworked template

--- configs/webpack.common.js
+++ configs/webpack.common.js
@@ -2,14 +2,15 @@ import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-  entry: path.resolve(import.meta.dirname,"../src/index.js"),
+  entry: path.resolve(import.meta.dirname, "../src/index.js"),
   output: {
     filename: "main.js",
-    path: path.resolve(import.meta.dirname, "..", 'dist'),
+    path: path.resolve(import.meta.dirname, "..", "dist"),
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(import.meta.dirname,"../src/template.html"),
+      template: path.resolve(import.meta.dirname, "../src/template.html"),
     }),
   ],
   module: {
@@ -21,6 +22,10 @@ export default {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/i,
+        use: ["babel-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
--- configs/webpack.dev.js
+++ configs/webpack.dev.js
@@ -1,9 +1,11 @@
+import path from "node:path";
+
 export default {
   mode: "development",
   devtool: "eval-source-map",
   devServer: {
     hot: true,
     port: 3000,
-    watchFiles: "../src/template.html",
+    watchFiles: path.resolve(import.meta.dirname, "../src/template.html"),
   },
 };
